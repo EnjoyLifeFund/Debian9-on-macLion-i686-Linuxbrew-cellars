85760f33305533984e639e29f9a5af9a5c6df372
Ralic Lo <raliclo@gmail.com>
2017-09-15 01:02:32 -0700

    Add more libs

---
+++ libxml2/2.9.5/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/kali/.linuxbrew/Cellar/libxml2/2.9.5/lib"
+XML2_LIBS="-lxml2 -L/home/kali/.linuxbrew/Cellar/zlib/1.2.11/lib -lz -L/home/kali/.linuxbrew/Cellar/xz/5.2.3/lib -llzma   -L/home/kali/.linuxbrew/Cellar/icu4c/59.1/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/kali/.linuxbrew/Cellar/libxml2/2.9.5/include/libxml2"
+MODULE_VERSION="xml2-2.9.5"
+
